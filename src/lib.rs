dd1f09fce3b74552e1d3cc65b516aa4146db4bba
mmacedoeu <contato@mmacedo.eu.org>
2021-08-04 18:29:04 -0300

    UPDATE cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+mod array;
 mod consts;
 mod de;
 pub mod error;
@@ -5,10 +6,9 @@ pub mod integers;
 mod read_ext;
 mod ser;
 mod write_ext;
-mod array;
 
-pub use de::{from_reader, Deserializer};
-pub use ser::{to_writer, Serializer};
 pub use array::OcamlArray;
+pub use de::{from_reader, Deserializer};
 pub use read_ext::ReadBinProtExt;
+pub use ser::{to_writer, Serializer};
 pub use write_ext::WriteBinProtExt;
