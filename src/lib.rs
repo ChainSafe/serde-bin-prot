87ebe86b7dfb6b6f29c4aac6b80f98d259086a55
mmacedoeu <contato@mmacedo.eu.org>
2021-08-04 18:15:59 -0300

    ADD closes #31

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,10 @@ pub mod integers;
 mod read_ext;
 mod ser;
 mod write_ext;
+mod array;
 
 pub use de::{from_reader, Deserializer};
 pub use ser::{to_writer, Serializer};
-
+pub use array::OcamlArray;
 pub use read_ext::ReadBinProtExt;
 pub use write_ext::WriteBinProtExt;
